ab9a1c2a0116b680b694e36dc046b21318d22355
Matthew Weser <matthew.weser@gmail.com>
2020-07-12 10:43:09 -0700

    Add Amplify auth theming

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {withAuthenticator, AmplifyTheme} from 'aws-amplify-react'
 import "./App.css";
 
 class App extends React.Component {
@@ -9,4 +10,25 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const theme = {
+  ...AmplifyTheme,
+  button: {
+    ...AmplifyTheme.button,
+    backgroundColor: 'cyan'
+  },
+  navBar: {
+    ...AmplifyTheme.navBar,
+    backgroundColor: "#413d41",
+    color: 'white'
+  },
+  sectionBody: {
+    ...AmplifyTheme.sectionBody,
+    padding: "5px"
+  },
+  sectionHeader: {
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: 'var(--squidInk)'
+  },
+}
+
+export default withAuthenticator(App, true, [], null, theme);
