f3741dd8932e33aece223e7cc4f946a0d82f691a
Matthew Weser <matthew.weser@gmail.com>
2020-07-12 15:24:50 -0700

    Add authenticator as hub

--- src/App.js
+++ src/App.js
@@ -1,34 +1,69 @@
 import React from "react";
-import {withAuthenticator, AmplifyTheme} from 'aws-amplify-react'
+import {Authenticator, AmplifyTheme} from 'aws-amplify-react'
+import {Auth, Hub} from 'aws-amplify'
 import "./App.css";
 
 class App extends React.Component {
-  state = {};
+    state = {
+        user: null
+    };
 
-  render() {
-    return <div>App</div>;
-  }
+    componentDidMount() {
+        this.getUserData()
+        Hub.listen('auth', this, 'onHubCapsule')
+    }
+
+    getUserData = async () => {
+        const user = await Auth.currentAuthenticatedUser()
+        user ? this.setState({user}) : this.setState({user: null})
+    }
+
+    onHubCapsule = capsule => {
+        switch (capsule.payload.event) {
+            case "signIn":
+                console.log('signed in')
+                this.getUserData()
+                break
+            case 'signUp':
+                console.log('signed up')
+                break
+            case 'signOut':
+                console.log('signed out')
+                this.setState({user: null})
+                break
+            default:
+                return
+        }
+    }
+
+    render() {
+        const {user} = this.state
+        return !user ? (
+            <Authenticator theme={theme}/>
+        ) : <div>App</div>;
+    }
 }
 
 const theme = {
-  ...AmplifyTheme,
-  button: {
-    ...AmplifyTheme.button,
-    backgroundColor: 'cyan'
-  },
-  navBar: {
-    ...AmplifyTheme.navBar,
-    backgroundColor: "#413d41",
-    color: 'white'
-  },
-  sectionBody: {
-    ...AmplifyTheme.sectionBody,
-    padding: "5px"
-  },
-  sectionHeader: {
-    ...AmplifyTheme.sectionHeader,
-    backgroundColor: 'var(--squidInk)'
-  },
+    ...AmplifyTheme,
+    button: {
+        ...AmplifyTheme.button,
+        backgroundColor: 'cyan'
+    },
+    navBar: {
+        ...AmplifyTheme.navBar,
+        backgroundColor: "#413d41",
+        color: 'white'
+    },
+    sectionBody: {
+        ...AmplifyTheme.sectionBody,
+        padding: "5px"
+    },
+    sectionHeader: {
+        ...AmplifyTheme.sectionHeader,
+        backgroundColor: 'var(--squidInk)'
+    },
 }
 
-export default withAuthenticator(App, true, [], null, theme);
+// export default withAuthenticator(App, true, [], null, theme);
+export default App
